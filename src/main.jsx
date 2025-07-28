fecab7269bbe4b6ad39c62fd9fa01dcd17e1a52b
deeksha1707 <2411cs010049@mallareddyuniversity.ac.in>
2025-07-28 12:07:46 +0530

    updated main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './VoteApp/Styles.css'
 import VotingApp from './VoteApp/Votingapp'
+// import VotingApp from './VoteApp/Votingapp'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
